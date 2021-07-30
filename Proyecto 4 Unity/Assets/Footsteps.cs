e8bf0493aa167969c2ef8ab00d4bc7eab9558a32
alecedur <alecu0825@gmail.com>
2021-07-29 22:15:23 -0600

    Final touches + animations

---
+++ Proyecto 4 Unity/Assets/Footsteps.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    // Use this for initialization
+    CharacterController cc;
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        if (cc.isGrounded == true && cc.velocity.magnitude == 0f && GetComponent<AudioSource>().isPlaying == true)
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
